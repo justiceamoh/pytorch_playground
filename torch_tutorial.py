46b477975db6ca5d8b4b2d6d7fb04e110425684c
Justice Amoh <justiceamoh@gmail.com>
2017-12-07 09:20:19 -0800

    initial commit

---
+++ torch_tutorial.py
@@ -0,0 +1,265 @@
+#####################
+## Numpy Neural Net #
+#####################
+import numpy as np
+
+# N is batch size; D_in is input dimension;
+# H is hidden dimension; D_out is output dimension.
+N, D_in, H, D_out = 64, 1000, 100, 10
+
+# Create random input and output data
+x = np.random.randn(N, D_in)
+y = np.random.randn(N, D_out)
+
+# Randomly initialize weights
+w1 = np.random.randn(D_in, H)
+w2 = np.random.randn(H, D_out)
+
+learning_rate = 1e-6
+for t in range(50):
+    # Forward pass: compute predicted y
+    h = x.dot(w1)
+    h_relu = np.maximum(h, 0)
+    y_pred = h_relu.dot(w2)
+
+    # Compute and print loss
+    loss = np.square(y_pred - y).sum()
+    print(t, loss)
+
+    # Backprop to compute gradients of w1 and w2 with respect to loss
+    grad_y_pred = 2.0 * (y_pred - y)
+    grad_w2 = h_relu.T.dot(grad_y_pred)
+    grad_h_relu = grad_y_pred.dot(w2.T)
+    grad_h = grad_h_relu.copy()
+    grad_h[h < 0] = 0
+    grad_w1 = x.T.dot(grad_h)
+
+    # Update weights
+    w1 -= learning_rate * grad_w1
+    w2 -= learning_rate * grad_w2
+
+
+#######################
+## PyTorch Neural Net #
+#######################
+import torch
+
+dtype = torch.FloatTensor
+# dtype = torch.cuda.FloatTensor # Uncomment this to run on GPU
+
+# Create random input and output data
+x = torch.randn(N, D_in).type(dtype)
+y = torch.randn(N, D_out).type(dtype)
+
+# Randomly initialize weights
+w1 = torch.randn(D_in, H).type(dtype)
+w2 = torch.randn(H, D_out).type(dtype)
+
+
+learning_rate = 1e-6
+for t in range(50):
+    # Forward pass: compute predicted y
+    h = x.mm(w1)
+    h_relu = h.clamp(min=0)
+    y_pred = h_relu.mm(w2)
+
+    # Compute and print loss
+    loss = (y_pred - y).pow(2).sum()
+    print(t, loss)
+
+    # Backprop to compute gradients of w1 and w2 with respect to loss
+    grad_y_pred = 2.0 * (y_pred - y)
+    grad_w2 = h_relu.t().mm(grad_y_pred)
+    grad_h_relu = grad_y_pred.mm(w2.t())
+    grad_h = grad_h_relu.clone()
+    grad_h[h < 0] = 0
+    grad_w1 = x.t().mm(grad_h)
+
+    # Update weights using gradient descent
+    w1 -= learning_rate * grad_w1
+    w2 -= learning_rate * grad_w2
+
+
+#################################
+## Backward Pass Using AutoGrad #
+#################################
+from torch.autograd import Variable
+
+'''
+Autograd is for automatic differentiation - easy way of doing backprop
+Wrap tensors in variable object:
+x      is variable
+x.data is Tensor
+x.grad is another variable holding gradient
+Variables have same API as Tensors in PyTorch
+'''
+
+# Create random Tensors to hold input and outputs, and wrap them in Variables.
+# Setting requires_grad=False indicates that we do not need to compute gradients
+# with respect to these Variables during the backward pass.
+x = Variable(torch.randn(N, D_in).type(dtype), requires_grad=False)
+y = Variable(torch.randn(N, D_out).type(dtype), requires_grad=False)
+
+# Similarly for Weights
+# Note, we need gradients so set requires_grad=True
+w1 = Variable(torch.randn(D_in, H).type(dtype), requires_grad=True)
+w2 = Variable(torch.randn(H, D_out).type(dtype), requires_grad=True)
+
+learning_rate = 1e-6
+for t in range(50):
+    t = next in range(50)
+    # Forward pass: compute predicted y using operations on Variables
+    # No need to keep references to intermediate since we are not doing
+    # backward pass by hand
+    y_pred = x.mm(w1).clamp(min=0).mm(w2)
+
+    # Compute and print loss using operations on Variables.
+    # Now loss is a Variable of shape (1,) and loss.data is a Tensor of shape
+    # (1,); loss.data[0] is a scalar value holding the loss.
+    loss = (y_pred - y).pow(2).sum()
+    print(t, loss.data[0])
+
+    # Use autograd to compute the backward pass. This call will compute the
+    # gradient of loss with respect to all Variables with requires_grad=True.
+    # After this call w1.grad and w2.grad will be Variables holding the gradient
+    # of the loss with respect to w1 and w2 respectively.
+    loss.backward()
+
+    # Update weights using gradient descent; w1.data and w2.data are Tensors,
+    # w1.grad and w2.grad are Variables and w1.grad.data and w2.grad.data are
+    # Tensors.
+    w1.data -= learning_rate * w1.grad.data
+    w2.data -= learning_rate * w2.grad.data
+
+    # Manually zero the gradients after updating weights
+    w1.grad.data.zero_()
+    w2.grad.data.zero_()
+
+
+#############################
+# Custom AutoGrad Operators #
+#############################
+
+# Custom ReLU
+class MyReLU(torch.autograd.Function):
+    """
+    Two steps for custom functions:
+    - Subclass torch.autograd.Function
+    - Implement forward and backward passes
+    """
+
+    def forward(self, input):
+        """
+        - argument is input Tensor, and return output Tensor
+        - can cache arbitrary Tensors for use in backward
+        """
+        self.save_for_backward(input)  # caching for use in backward pass
+        return input.clamp(min=0)
+
+    def backward(self, grad_output):
+        """
+        In the backward pass we receive a Tensor containing the gradient of the loss
+        with respect to the output, and we need to compute the gradient of the loss
+        with respect to the input.
+        """
+        input, = self.saved_tensors
+        grad_input = grad_output.clone()
+        grad_input[input < 0] = 0
+        return grad_input
+
+x  = Variable(torch.randn(N, D_in).type(dtype), requires_grad=False)
+y  = Variable(torch.randn(N, D_out).type(dtype), requires_grad=False)
+w1 = Variable(torch.randn(D_in, H).type(dtype), requires_grad=True)
+w2 = Variable(torch.randn(H, D_out).type(dtype), requires_grad=True)
+
+learning_rate = 1e-6
+for t in range(50):
+    # Construct an instance of our MyReLU class to use in our network
+    relu = MyReLU()
+    y_pred = relu(x.mm(w1)).mm(w2)
+    loss = (y_pred - y).pow(2).sum()
+    print(t, loss.data[0])
+    loss.backward()
+
+    # Update weights using gradient descent
+    w1.data -= learning_rate * w1.grad.data
+    w2.data -= learning_rate * w2.grad.data
+
+    # Manually zero the gradients after updating weights
+    w1.grad.data.zero_()
+    w2.grad.data.zero_()
+
+#===========================================================
+
+#############
+# nn Module #
+#############
+# Create IO Tensors
+x = Variable(torch.randn(N, D_in))
+y = Variable(torch.randn(N, D_out), requires_grad=False)
+
+
+# Use the nn package to define our model as a sequence of layers. 
+model = torch.nn.Sequential(
+    torch.nn.Linear(D_in, H),
+    torch.nn.ReLU(),
+    torch.nn.Linear(H, D_out),
+)
+
+# The nn package also contains definitions of popular loss functions; in this
+# case we will use Mean Squared Error (MSE) as our loss function.
+loss_fn = torch.nn.MSELoss(size_average=False)
+
+learning_rate = 1e-4
+for t in range(50):
+    y_pred = model(x)
+    loss = loss_fn(y_pred, y)
+    print(t, loss.data[0])
+
+    # Zero the gradients before running the backward pass.
+    model.zero_grad()
+
+    # Backward pass: compute gradient of the loss with respect to all the learnable
+    # parameters of the model. Internally, the parameters of each Module are stored
+    # in Variables with requires_grad=True, so this call will compute gradients for
+    # all learnable parameters in the model.
+    loss.backward()
+
+    # Update the weights using gradient descent. Each parameter is a Variable, so
+    # we can access its data and gradients like we did before.
+    for param in model.parameters():
+        param.data -= learning_rate * param.grad.data
+
+
+
+#########################
+# nn Module using optim #
+#########################
+model = torch.nn.Sequential(
+    torch.nn.Linear(D_in, H),
+    torch.nn.ReLU(),
+    torch.nn.Linear(H, D_out),
+)
+loss_fn = torch.nn.MSELoss(size_average=False)
+
+#using the optim package
+learning_rate = 1e-4
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for t in range(50):
+    y_pred = model(x)
+    loss = loss_fn(y_pred, y)
+    print(t, loss.data[0])
+
+    optimizer.zero_grad()
+
+    loss.backward()
+
+    # Calling the step function on an Optimizer makes 
+    # an update to its parameters
+    optimizer.step()
+
+
+
+
+
